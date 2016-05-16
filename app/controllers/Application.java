601cd22b6d4a1672a07067cee930b4f1af1951f8
Sumit Shyamsukha <sumitzster@gmail.com>
2016-05-16 17:34:56 -0400

    init commit

---
+++ app/controllers/Application.java
@@ -0,0 +1,43 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+import play.db.jpa.*;
+import views.html.*;
+import models.Person;
+import play.data.FormFactory;
+import javax.inject.Inject;
+import java.util.List;
+
+import static play.libs.Json.*;
+
+public class Application extends Controller {
+
+    @Inject
+    FormFactory formFactory;
+
+    public Result index() {
+        return ok(index.render());
+    }
+
+    public Result hello() {
+        return ok("hello, world!");
+    }
+
+    public Result hello_name(String name) {
+        return ok("hello, " + name);
+    }
+
+    @Transactional
+    public Result addPerson() {
+        Person person = formFactory.form(Person.class).bindFromRequest().get();
+        JPA.em().persist(person);
+        return redirect(routes.Application.index());
+    }
+
+    @Transactional(readOnly = true)
+    public Result getPersons() {
+        List<Person> persons = (List<Person>) JPA.em().createQuery("select p from Person p").getResultList();
+        return ok(toJson(persons));
+    }
+}
